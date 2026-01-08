78b7455f7e8a82cb4c5823c3f164176564f23345
brebierod <bierodbre@gmail.com>
2026-01-07 23:44:09 -0500

    Link javascript and write pseudocode

---
+++ script.js
@@ -0,0 +1,24 @@
+//Prompt asks user to type rock, paper, scissors.
+//Computer randomly chooses rock, paper scissors.
+    //Computer picks number between 1 and 3.
+    //1, 2, 3 convert to rock, paper, scissor
+//Code goes to choosesRock function if user chose rock.
+    //Fetch computer response.
+    //If computer chose paper, computer win prints.
+    //If computer chose rock, a draw prints.
+    //If computer chose scissors, user win prints.
+//Code goes to choosesPaper function if user chose paper.
+    //Fetch computer response.
+    //If computer chose paper, a draw prints.
+    //If computer chose rock, user win prints.
+    //If computer chose scissors, computer win prints.
+//Code goes to chooses Scissors function if user chose scissors.
+    //Fetch computer response.
+    //If computer chose paper, user win prints.
+    //If computer chose rock, computer win prints.
+    //If computer chose scissors, a draw prints.
+//If user typed invalid response, print error and try again.
+
+function getComputerChoice() {
+    return
+} 
